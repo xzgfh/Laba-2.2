8f9652ff7ce3baf0f991232395208acbf21cbdcb
xzgfh <User@WIN-Q06VGDO4GD9>
2021-12-21 15:27:29 +0300

    Добавьте файлы проекта.

---
+++ Laba 2.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                Vector3D a = new Vector3D(1, 1, 1);
+                Vector3D b = new Vector3D(2, 2, 2);
+                Vector3D c = a + b;
+                Vector3D d = a - b;
+                double r = a * b;
+                Vector3D h = 3 * a;
+                double k = a.length;
+                Console.WriteLine(c);
+
+            
+        }
+    }
+}
---
+++ Laba 2.2/Vector3D.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_2._2
+{
+    class Vector3D
+    {
+        public int x { get; set; }
+
+        public int y { get; set; }
+
+        public int z { get; set; }
+
+        public Vector3D(int x, int y, int z)
+
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static Vector3D operator +(Vector3D a, Vector3D b)
+        {
+            return new Vector3D(a.x + b.x, a.y + b.y, a.z + b.z);
+        }
+
+        public static Vector3D operator -(Vector3D a, Vector3D b)
+        {
+            return new Vector3D(a.x - b.x, a.y - b.y, a.z - b.z);
+        }
+
+        public static double operator *(Vector3D a, Vector3D b)
+        {
+            return (a.x * b.x + a.y * b.y + a.z * b.z);
+        }
+
+        public static Vector3D operator *(int a, Vector3D b)
+        {
+
+            return new Vector3D(a * b.x, a * b.y, a * b.z);
+        }
+
+        public double length
+        {
+            get { return Math.Sqrt(x * x + y * y + z * z); }
+        }
+    }
+}
